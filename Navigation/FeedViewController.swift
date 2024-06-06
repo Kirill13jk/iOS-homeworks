018400f5023da16f213cf1c6573a891b636c890c
Kirill <kirill13happy@gmail.com>
2024-06-06 16:52:53 +0500

    fearure/iosui-8 4

--- Navigation/FeedViewController.swift
+++ Navigation/FeedViewController.swift
@@ -4,7 +4,7 @@ class FeedViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .systemPurple
         
         // Создание UIStackView
         let stackView = UIStackView()
@@ -14,12 +14,19 @@ class FeedViewController: UIViewController {
         
         // Создание кнопок
         let button1 = UIButton(type: .system)
-        button1.setTitle("Open Post 1", for: .normal)
+        button1.setTitle("Open Post Super Man", for: .normal)
         button1.addTarget(self, action: #selector(openPost), for: .touchUpInside)
         
+        button1.layer.backgroundColor = UIColor.white.cgColor
+        button1.layer.cornerRadius = 12
+
+        
         let button2 = UIButton(type: .system)
-        button2.setTitle("Open Post 2", for: .normal)
+        button2.setTitle("Open Post Spider Man", for: .normal)
         button2.addTarget(self, action: #selector(openPost), for: .touchUpInside)
+        
+        button2.layer.backgroundColor = UIColor.systemGray5.cgColor
+        button2.layer.cornerRadius = 12
         
         // Добавление кнопок в UIStackView
         stackView.addArrangedSubview(button1)
@@ -30,6 +37,11 @@ class FeedViewController: UIViewController {
         
         // Установка ограничений для UIStackView
         NSLayoutConstraint.activate([
+            button1.widthAnchor.constraint(equalToConstant: 200),
+            button1.heightAnchor.constraint(equalToConstant: 50),
+            
+            button2.heightAnchor.constraint(equalToConstant: 50),
+            
             stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
         ])
