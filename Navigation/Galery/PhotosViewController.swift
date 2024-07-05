5a5fe0725843984249ac9639b4e95b2648be56c8
Kirill <onlyjesus13@icloud.com>
2024-07-05 15:58:41 +0500

    iosui-5 last commit

--- Navigation/Galery/PhotosViewController.swift
+++ Navigation/Galery/PhotosViewController.swift
@@ -2,18 +2,19 @@ import UIKit
 import iOSIntPackage
 
 // Контроллер для отображения фото галереи
-class PhotosViewController: UIViewController {
+class PhotosViewController: UIViewController, ImageLibrarySubscriber {
     
     // Создаем UICollectionView для отображения изображений
     private let collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
-        let itemSpacing: CGFloat = 10
-        let itemsPerRow: CGFloat = 3
+        let itemSpacing: CGFloat = 8 // Расстояние между элементами
+        let itemsPerRow: CGFloat = 3 // Количество элементов в строке
+        // Расчет ширины элемента с учетом отступов и количества элементов в строке
         let width = (UIScreen.main.bounds.width - (itemsPerRow + 1) * itemSpacing) / itemsPerRow
         layout.itemSize = CGSize(width: width, height: width)
         layout.sectionInset = UIEdgeInsets(top: itemSpacing, left: itemSpacing, bottom: itemSpacing, right: itemSpacing)
-        layout.minimumLineSpacing = itemSpacing
-        layout.minimumInteritemSpacing = itemSpacing
+        layout.minimumLineSpacing = itemSpacing // Минимальное расстояние между строками
+        layout.minimumInteritemSpacing = itemSpacing // Минимальное расстояние между элементами в строке
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.backgroundColor = .white
         collectionView.translatesAutoresizingMaskIntoConstraints = false
@@ -48,7 +49,12 @@ class PhotosViewController: UIViewController {
         // Инициализация imagePublisher и подписка на обновления
         imagePublisher = ImagePublisherFacade()
         imagePublisher?.subscribe(self)
-        imagePublisher?.addImagesWithTimer(time: 0.5, repeat: 10)
+        
+        // Передаем пользовательские изображения
+        let userImages: [UIImage] = loadUserImages()
+        // Проверка, что все 20 изображений загружены
+        assert(userImages.count == 20, "Должно быть загружено 20 изображений, но загружено \(userImages.count)")
+        imagePublisher?.addImagesWithTimer(time: 0.5, repeat: userImages.count, userImages: userImages)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -59,6 +65,26 @@ class PhotosViewController: UIViewController {
     // Отменяем подписку при деинициализации контроллера
     deinit {
         imagePublisher?.removeSubscription(for: self)
+    }
+    
+    // Метод для загрузки пользовательских изображений
+    private func loadUserImages() -> [UIImage] {
+        var images = [UIImage]()
+        for i in 1...20 {
+            if let image = UIImage(named: "photo\(i)") {
+                images.append(image)
+            }
+        }
+        return images
+    }
+    
+    // Метод для получения изображений от ImagePublisherFacade
+    func receive(images: [UIImage]) {
+        // Добавляем уникальные изображения в массив, удаляя дубликаты
+        let uniqueReceivedImages = images.unique()
+        self.images.append(contentsOf: uniqueReceivedImages)
+        self.images = self.images.unique()
+        self.collectionView.reloadData() // Перезагружаем данные коллекции
     }
 }
 
@@ -76,13 +102,15 @@ extension PhotosViewController: UICollectionViewDataSource, UICollectionViewDele
     }
 }
 
-// Подписываем PhotosViewController на протокол ImageLibrarySubscriber
-extension PhotosViewController: ImageLibrarySubscriber {
-    func receive(images: [UIImage]) {
-        // Обновление коллекции изображений на главном потоке
-        DispatchQueue.main.async {
-            self.images.append(contentsOf: images) // Добавляем новые изображения в массив
-            self.collectionView.reloadData() // Перезагружаем данные коллекции
+// Расширение для удаления дубликатов из массива
+extension Array where Element: Equatable {
+    func unique() -> [Element] {
+        var uniqueValues: [Element] = []
+        for value in self {
+            if !uniqueValues.contains(value) {
+                uniqueValues.append(value)
+            }
         }
+        return uniqueValues
     }
 }
