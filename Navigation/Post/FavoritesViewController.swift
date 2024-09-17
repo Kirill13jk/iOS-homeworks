a892180a7a5e22abb2b1db246d03ce15e2fbfec4
Kirill <onlyjesus13@icloud.com>
2024-09-17 11:42:31 +0500

    core-data-3 commit

--- Navigation/Post/FavoritesViewController.swift
+++ Navigation/Post/FavoritesViewController.swift
@@ -3,20 +3,21 @@ import CoreData
 import StorageService
 
 
-class FavoritesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class FavoritesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, NSFetchedResultsControllerDelegate {
+
     weak var coordinator: FavoriteCoordinator?
-
     var tableView: UITableView!
-    var favoritePosts: [FavoritePostE] = []
-    
-    var currentAuthorFilter: String?
 
+    // Добавляем NSFetchedResultsController
+    var fetchedResultsController: NSFetchedResultsController<FavoritePostE>!
+
+    var currentAuthorFilter: String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
-        fetchFavoritePosts()
-        
+        setupFetchedResultsController()
+
         let filterButton = UIBarButtonItem(title: "Фильтр", style: .plain, target: self, action: #selector(showFilterAlert))
         let clearFilterButton = UIBarButtonItem(title: "Сбросить", style: .plain, target: self, action: #selector(clearFilter))
         navigationItem.rightBarButtonItems = [clearFilterButton, filterButton]
@@ -24,20 +25,11 @@ class FavoritesViewController: UIViewController, UITableViewDataSource, UITableV
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        fetchFavoritePosts() // Обновляем данные при появлении экрана
-    }
-    
-    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-
-        let deleteAction = UIContextualAction(style: .destructive, title: "Удалить") { [weak self] (action, view, completionHandler) in
-            self?.deleteFavoritePost(at: indexPath)
-            completionHandler(true)
-        }
-
-        let configuration = UISwipeActionsConfiguration(actions: [deleteAction])
-        return configuration
+        try? fetchedResultsController.performFetch()
+        tableView.reloadData()
     }
 
+    // MARK: - Настройка таблицы
     func setupTableView() {
         tableView = UITableView(frame: view.bounds)
         tableView.register(PostTableViewCell.self, forCellReuseIdentifier: "PostCell")
@@ -46,36 +38,44 @@ class FavoritesViewController: UIViewController, UITableViewDataSource, UITableV
         view.addSubview(tableView)
     }
 
-    func fetchFavoritePosts() {
+    // MARK: - Настройка NSFetchedResultsController
+    func setupFetchedResultsController() {
         let context = CoreDataManager.shared.context
         let fetchRequest: NSFetchRequest<FavoritePostE> = FavoritePostE.fetchRequest()
 
-        // Применяем фильтр, если нужно
+        // Применяем фильтр по автору, если он есть
         if let authorFilter = currentAuthorFilter {
             fetchRequest.predicate = NSPredicate(format: "author == %@", authorFilter)
         }
 
+        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+
+        fetchedResultsController = NSFetchedResultsController(
+            fetchRequest: fetchRequest,
+            managedObjectContext: context,
+            sectionNameKeyPath: nil,
+            cacheName: nil
+        )
+
+        fetchedResultsController.delegate = self
+
         do {
-            favoritePosts = try context.fetch(fetchRequest)
-            tableView.reloadData()
+            try fetchedResultsController.performFetch()
         } catch {
-            print("Ошибка при загрузке данных: \(error)")
+            print("Ошибка при выполнении запроса: \(error)")
         }
     }
 
-
     // MARK: - UITableViewDataSource методы
-
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return favoritePosts.count
+        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell", for: indexPath) as! PostTableViewCell
-        let favoritePost = favoritePosts[indexPath.row]
+        let favoritePost = fetchedResultsController.object(at: indexPath)
 
-        // Создаем объект Post для переиспользования метода configure
         let post = Post(
             title: favoritePost.title ?? "",
             author: favoritePost.author ?? "",
@@ -90,13 +90,21 @@ class FavoritesViewController: UIViewController, UITableViewDataSource, UITableV
         return cell
     }
 
+    // MARK: - Удаление поста
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: "Удалить") { [weak self] (action, view, completionHandler) in
+            self?.deleteFavoritePost(at: indexPath)
+            completionHandler(true)
+        }
+
+        return UISwipeActionsConfiguration(actions: [deleteAction])
+    }
+
     func deleteFavoritePost(at indexPath: IndexPath) {
-        let postToDelete = favoritePosts[indexPath.row]
+        let postToDelete = fetchedResultsController.object(at: indexPath)
         let context = CoreDataManager.shared.context
 
         context.delete(postToDelete)
-        favoritePosts.remove(at: indexPath.row)
-        tableView.deleteRows(at: [indexPath], with: .automatic)
 
         do {
             try context.save()
@@ -105,7 +113,55 @@ class FavoritesViewController: UIViewController, UITableViewDataSource, UITableV
             print("Ошибка при удалении поста: \(error)")
         }
     }
-    
+
+    // MARK: - NSFetchedResultsControllerDelegate методы
+    func controllerWillChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.beginUpdates()
+    }
+
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>,
+                    didChange anObject: Any,
+                    at indexPath: IndexPath?,
+                    for type: NSFetchedResultsChangeType,
+                    newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            if let newIndexPath = newIndexPath {
+                tableView.insertRows(at: [newIndexPath], with: .automatic)
+            }
+        case .delete:
+            if let indexPath = indexPath {
+                tableView.deleteRows(at: [indexPath], with: .automatic)
+            }
+        case .update:
+            if let indexPath = indexPath {
+                let cell = tableView.cellForRow(at: indexPath) as! PostTableViewCell
+                let favoritePost = fetchedResultsController.object(at: indexPath)
+                let post = Post(
+                    title: favoritePost.title ?? "",
+                    author: favoritePost.author ?? "",
+                    description: favoritePost.content ?? "",
+                    image: favoritePost.imageData != nil ? UIImage(data: favoritePost.imageData!) : nil,
+                    likes: Int(favoritePost.likes),
+                    views: Int(favoritePost.views)
+                )
+                cell.configure(with: post, isFavorite: true)
+            }
+        case .move:
+            if let indexPath = indexPath, let newIndexPath = newIndexPath {
+                tableView.deleteRows(at: [indexPath], with: .automatic)
+                tableView.insertRows(at: [newIndexPath], with: .automatic)
+            }
+        @unknown default:
+            fatalError("Неизвестный тип изменения в NSFetchedResultsController")
+        }
+    }
+
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        tableView.endUpdates()
+    }
+
+    // MARK: - Фильтр
     @objc func showFilterAlert() {
         let alert = UIAlertController(title: "Фильтр по автору", message: "Введите имя автора", preferredStyle: .alert)
         alert.addTextField { (textField) in
@@ -115,7 +171,8 @@ class FavoritesViewController: UIViewController, UITableViewDataSource, UITableV
         let applyAction = UIAlertAction(title: "Применить", style: .default) { [weak self] _ in
             if let authorName = alert.textFields?.first?.text, !authorName.isEmpty {
                 self?.currentAuthorFilter = authorName
-                self?.fetchFavoritePosts()
+                self?.setupFetchedResultsController()
+                self?.tableView.reloadData()
             }
         }
 
@@ -129,8 +186,7 @@ class FavoritesViewController: UIViewController, UITableViewDataSource, UITableV
 
     @objc func clearFilter() {
         currentAuthorFilter = nil
-        fetchFavoritePosts()
+        setupFetchedResultsController()
+        tableView.reloadData()
     }
-
 }
-
